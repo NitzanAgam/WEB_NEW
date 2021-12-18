580bd87ac54e879b8d6066ad48646c3def2b9885
Nitzan Agam <nitzanagam@gmail.com>
2021-12-18 13:07:21 +0200

    Exercise 8

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, redirect, url_for, render_template
+from flask import request, session
 
 app = Flask(__name__)
+app.secret_key = '123'
 
 @app.route('/')
 @app.route('/home')
@@ -33,8 +35,40 @@ def about_func():
 
 @app.route('/catalog')
 def catalog_func():
-    return render_template('catalog.html', color='green')
+    if 'product' in request.args:
+        product = request.args['product']
+        size = request.args['size']
+        return render_template('catalog.html',p_name=product,p_size=size, color='green')
+    return render_template('catalog.html')
+
+
+@app.route('/login',methods=['GET', 'POST'])
+def login_func():
+    if request.method == 'GET':
+        return render_template('login.html')
+    if request.method == 'POST':
+        # DB
+        session.username = request.form['username']
+        session.password = request.form['password']
+        return render_template('index.html', username=session.username)
+
+
+@app.route('/logout')
+def logout_func():
+    session['username'] = ''
+    return render_template('index.html')
+
+
+@app.route('/assignment8')
+def hobbies_func():
+    currentUser = 'Nitzan'
+    return render_template('assignment8.html',
+                           name=currentUser,
+                           travel={'New York', 'Bangkok', 'London', 'Barcelona'},
+                           Dancing={'Folklore', 'Jazz', 'Mainstream'},
+                           bShows={'Mama Mia', 'Frozen', 'Kinky Boots'})
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
