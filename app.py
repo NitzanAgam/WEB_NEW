901b20105c5c9ef590888aa8b4bf34850219aff3
Nitzan Agam <nitzanagam@gmail.com>
2021-12-25 12:12:49 +0200

    Exercise 9

--- app.py
+++ app.py
@@ -56,6 +56,7 @@ def login_func():
 @app.route('/logout')
 def logout_func():
     session['username'] = ''
+    session['LoggedIn'] = False
     return render_template('index.html')
 
 
@@ -67,6 +68,37 @@ def hobbies_func():
                            travel={'New York', 'Bangkok', 'London', 'Barcelona'},
                            Dancing={'Folklore', 'Jazz', 'Mainstream'},
                            bShows={'Mama Mia', 'Frozen', 'Kinky Boots'})
+
+
+@app.route('/assignment9', methods=['GET', 'POST'])
+def assignment9():
+    username = ''
+    MyUsers = [
+        {'id': 1, 'email': "nitzan.agam@gmail.in", 'firstname': "Nitzan", 'lastname': "Agam"},
+        {'id': 2, 'email': "kimkardash@gmail.in", 'firstname': "Kim", 'lastname': "Kardashian"},
+        {'id': 3, 'email': "Khloe.kardash@gmail.in", 'firstname': "Khloe", 'lastname': "Kardashian"},
+        {'id': 4, 'email': "Kortney.kardash@gmail.in", 'firstname': "Kortney", 'lastname': "Kardashian"},
+        {'id': 5, 'email': "kylie.jenner@gmail.in", 'firstname': "Kylie", 'lastname': "Kardashian"},
+        {'id': 6, 'email': "kendel.jenner@gmail.in", 'firstname': "Kendel", 'lastname': "Kardashian"}
+    ]
+    firstname = ''
+    if request.method == 'GET':
+        if 'firstname' in request.args:
+            firstname = request.args['firstname']
+    elif request.method == 'POST':
+        if 'username' in request.form:
+            username = request.form['username']
+            session['LoggedIn'] = True
+            session['username'] = username
+        else:
+            session['LoggedIn'] = False
+            session['username'] = ''
+            username = ''
+    return render_template('Assignment9.html',
+                           MyUsers=MyUsers,
+                           username=username,
+                           firstname=firstname,
+                           request_method=request.method)
 
 
 if __name__ == '__main__':
